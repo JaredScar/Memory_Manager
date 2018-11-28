54bfe457f8ec1edc7dc89c68c29252bb40707148
TheWolfBadger <TheWolfBadger@gmail.com>
2018-11-27 20:42:43 -0500

    - Added 'buttons' package - Added Button classes (implementing ActionListener)

---
+++ src/com/jaredscarito/memory_manager/api/buttons/AddButton.java
@@ -0,0 +1,11 @@
+package com.jaredscarito.memory_manager.api.buttons;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddButton implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //TODO
+    }
+}
