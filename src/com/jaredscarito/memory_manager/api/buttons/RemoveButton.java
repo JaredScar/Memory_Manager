36b32d310a6c47a48a532a927965560436ac1803
raymacnyg <45398420+raymacnyg@users.noreply.github.com>
2018-11-30 17:17:26 -0500

    Update RemoveButton.java

--- src/com/jaredscarito/memory_manager/api/buttons/RemoveButton.java
+++ src/com/jaredscarito/memory_manager/api/buttons/RemoveButton.java
@@ -2,11 +2,20 @@ package com.jaredscarito.memory_manager.api.buttons;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import com.jaredscarito.memory_manager.api.API;
+import javax.swing.*;
 
 public class RemoveButton implements EventHandler<MouseEvent> {
 
     @Override
     public void handle(MouseEvent event) {
-        //TODO
+        String Pid = API.getInstance().getSelectedPid();
+
+        if(API.getInstance().removeBlock(Pid) == false)
+        {
+            JOptionPane.showMessageDialog(null, "Process does not exist memory");
+        }
+
+
     }
 }
